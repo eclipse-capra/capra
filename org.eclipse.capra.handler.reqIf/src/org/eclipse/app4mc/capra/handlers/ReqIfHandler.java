bfdbbf4880250734f7d8ae278108fca304d0869b
Salome Maro <salomehonest@gmail.com>
2016-10-06 17:13:44 +0200

    added a reqIF handler

---
+++ org.eclipse.capra.handler.reqIf/src/org/eclipse/app4mc/capra/handlers/ReqIfHandler.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *  Copyright (c) 2016 Chalmers | Gothenburg University, rt-labs and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *   Contributors:
+ *      Chalmers|Gothenburg University and rt-labs - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.app4mc.capra.handlers;
+
+import org.eclipse.capra.core.handlers.ArtifactHandler;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+public class ReqIfHandler implements ArtifactHandler {
+
+	@Override
+	public boolean canHandleSelection(Object selection) {
+		return selection instanceof IStructuredSelection;
+	}
+
+	@Override
+	public EObject getEObjectForSelection(Object selection, EObject artifactModel) {
+		IStructuredSelection sel = (IStructuredSelection) selection;
+		EObject obj = EObject.class.cast(sel.getFirstElement());
+
+		return obj;
+	}
+
+	@Override
+	public Object resolveArtifact(EObject artifact) {
+		
+		return artifact;
+	}
+
+}
