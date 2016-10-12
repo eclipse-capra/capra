c7d17e1e231e704accc1103560f5b0a221039043
Dusan <kalanj@chalmers.se>
2016-10-12 17:27:49 +0200

    Addressed the comments from the previous push: replaced the String filePath with a io.File object in the office project.
    Change-Id: Ibb6ed4a3fe7309f3a0240483514d92f1dc55cd21
    Signed-off-by: Dusan Kalanj <kalanj@chalmers.se>

---
+++ org.eclipse.capra.handler.office/src/org/eclipse/capra/handler/office/OfficeHandler.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Chalmers | University of Gothenburg, rt-labs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * 	   Chalmers | University of Gothenburg and rt-labs - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+package org.eclipse.capra.handler.office;
+
+import org.eclipse.capra.core.adapters.ArtifactMetaModelAdapter;
+import org.eclipse.capra.core.handlers.ArtifactHandler;
+import org.eclipse.capra.core.helpers.ExtensionPointHelper;
+import org.eclipse.capra.ui.office.objects.CapraOfficeObject;
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * A handler to create trace links from and to content of Office files.
+ * 
+ * @author Dusan Kalanj
+ * 
+ */
+public class OfficeHandler implements ArtifactHandler {
+
+	@Override
+	public boolean canHandleSelection(Object selection) {
+		return selection instanceof CapraOfficeObject;
+	}
+
+	@Override
+	public EObject getEObjectForSelection(Object selection, EObject artifactModel) {
+		// Returns the EObject corresponding to the input object if the input is
+		// an EObject, or if it is Adaptable to an EObject
+		CapraOfficeObject officeObject = (CapraOfficeObject) selection;
+		if (officeObject != null) {
+			ArtifactMetaModelAdapter adapter = ExtensionPointHelper.getArtifactWrapperMetaModelAdapter().get();
+			// TODO here artifactName is the same as the row/paragraph
+			// description. Should it be different?
+			EObject wrapper = adapter.createArtifact(artifactModel, this.getClass().getName(), officeObject.getUri(),
+					officeObject.toString());
+			return wrapper;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public Object resolveArtifact(EObject artifact) {
+		// TODO Decide on how to resolve the artifact.
+		// Just return the appropriate row / requirement or display it in the
+		// OfficeView?
+		return null;
+	}
+
+}
