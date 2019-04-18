8fac44c3a676c69b8262804f9dacd99a1e14c6e5
Salome Maro <salomehonest@gmail.com>
2019-04-18 07:26:13 +0200

    Bug 513053 - Provide more meaningful display names for reqif elements
    Since users can define custom attributes for their ReqIf requirements,
    a preference page is added to allow the users to provide the name of
    the attribute they wish to use in the display name.
    If this is not provided, Capra will use the first attribute defined.
    
    Change-Id: I22091ebba1db24773977919077bf2b6876c00cea

--- bundles/org.eclipse.capra.handler.reqIf/src/org/eclipse/capra/handler/reqif/ReqIfHandler.java
+++ bundles/org.eclipse.capra.handler.reqIf/src/org/eclipse/capra/handler/reqif/ReqIfHandler.java
@@ -18,10 +18,14 @@ import java.util.List;
 
 import org.eclipse.capra.core.adapters.Connection;
 import org.eclipse.capra.core.handlers.AbstractArtifactHandler;
+import org.eclipse.capra.handler.reqif.preferences.ReqifPreferences;
 import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.rmf.reqif10.AttributeValue;
 import org.eclipse.rmf.reqif10.SpecHierarchy;
 import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.common.util.ReqIF10Util;
 
 public class ReqIfHandler extends AbstractArtifactHandler<SpecHierarchy> {
 
@@ -39,8 +43,22 @@ public class ReqIfHandler extends AbstractArtifactHandler<SpecHierarchy> {
 
 	@Override
 	public String getDisplayName(SpecHierarchy spec) {
+		IEclipsePreferences preference = ReqifPreferences.getPreferences();
+		String idAttribute = preference.get(ReqifPreferences.REQIF_ID_ATTRIBUTE,
+				ReqifPreferences.REQIF_ID_ATTRIBUTE_DEFAULT);
 		SpecObject specObject = spec.getObject();
-		return specObject.getIdentifier();
+		if (specObject != null) {
+			AttributeValue attributeValue = ReqIF10Util.getAttributeValueForLabel(specObject, idAttribute);
+			if (attributeValue != null) {
+				return ReqIF10Util.getTheValue(attributeValue).toString();
+			} else {
+				// as a default fall back use the first column as an ID
+				// column
+				AttributeValue defaultAttributeValue = specObject.getValues().get(0);
+				return ReqIF10Util.getTheValue(defaultAttributeValue).toString();
+			}
+		} else // empty row is selected
+			return "";
 	}
 
 	@Override
