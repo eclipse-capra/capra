f90745d192b1fd60d324af42146d941d55921eae
Jan-Philipp Steghöfer <ec@splashstudio.com>
2021-06-10 10:03:23 +0200

    Matrix shows origins in rows, targets in columns
    The matrix view previously always showed a symmetrical view of the
    artifacts, meaning that rows and columns were identical. In addition,
    they were sorted by their appearance in the trace links that were
    shown. This led to a hodgepodge of artifacts and made it harder to
    identify the relevant parts.
    
    This patch changes the behaviour as follows:
    * All origins related to artifacts selected somewhere in Eclipse show
      up as rows.
    * All targets from the trace links that have the selected elements as
      origins show up in columns.
    * The artifacts in both columns and rows are sorted by the class name
      of their respective artifact handlers. This effectively groups
      artifacts of the same type together.
    
    This makes it easier to see if, e.g., all selected requirements are
    covered by test cases or if all failures are associated to hazards.
    
    Change-Id: I6274290127af44ad6c9c1932481d1e57d0bbb6b4

--- bundles/org.eclipse.capra.ui.matrix/src/org/eclipse/capra/ui/matrix/TraceabilityMatrixDataProvider.java
+++ bundles/org.eclipse.capra.ui.matrix/src/org/eclipse/capra/ui/matrix/TraceabilityMatrixDataProvider.java
@@ -15,15 +15,26 @@
 package org.eclipse.capra.ui.matrix;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.eclipse.capra.core.adapters.Connection;
+import org.eclipse.capra.core.adapters.IPersistenceAdapter;
 import org.eclipse.capra.core.adapters.ITraceabilityInformationModelAdapter;
+import org.eclipse.capra.core.handlers.IArtifactHandler;
+import org.eclipse.capra.core.helpers.ArtifactHelper;
 import org.eclipse.capra.core.helpers.EMFHelper;
-import org.eclipse.capra.core.helpers.TraceHelper;
+import org.eclipse.capra.core.helpers.EditingDomainHelper;
+import org.eclipse.capra.core.helpers.ExtensionPointHelper;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 
 /**
@@ -36,6 +47,9 @@ import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
  */
 public class TraceabilityMatrixDataProvider implements IDataProvider {
 
+	private ResourceSet resourceSet = EditingDomainHelper.getResourceSet();
+	private IPersistenceAdapter persistenceAdapter = ExtensionPointHelper.getPersistenceAdapter().get();
+
 	private class EntryData {
 		public EObject artifact;
 		public List<Connection> connections;
@@ -45,8 +59,81 @@ public class TraceabilityMatrixDataProvider implements IDataProvider {
 		}
 	}
 
+	/**
+	 * A comparator that provides a lexicographical ordering of the class names of
+	 * the artifact handlers that take care of the {@link EObject} instances.
+	 * 
+	 * Note: this comparator imposes orderings that are inconsistent with equals
+	 */
+	private class ArtifactHandlerClassNameComparator implements Comparator<Object> {
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public int compare(Object o1, Object o2) {
+			if (o1 == null && o2 == null) {
+				return 0;
+			} else if (o1 == null || o2 == null) {
+				return o1 == null ? 1 : -1;
+			}
+			EObject artifactModel = persistenceAdapter.getArtifactWrappers(resourceSet);
+			ArtifactHelper artifactHelper = new ArtifactHelper(artifactModel);
+			IArtifactHandler<Object> o1Handler = (IArtifactHandler<Object>) artifactHelper
+					.getHandler(artifactHelper.unwrapWrapper(o1)).orElse(null);
+			IArtifactHandler<Object> o2Handler = (IArtifactHandler<Object>) artifactHelper
+					.getHandler(artifactHelper.unwrapWrapper(o2)).orElse(null);
+			if (o1Handler != null && o2Handler != null) {
+				return o1Handler.getClass().getSimpleName().compareTo(o2Handler.getClass().getSimpleName());
+			} else if (o1Handler == null && o2Handler == null) {
+				return 0;
+			} else {
+				return o1Handler == null ? 1 : -1;
+			}
+		}
+
+	}
+
 	private List<EntryData> rows = new ArrayList<>();
 	private List<EntryData> columns = new ArrayList<>();
+
+	/**
+	 * Returns all unique artifacts of the connections as defined by {@code func},
+	 * sorted by the name of the {@link IArtifactHandler} that deals with them.
+	 * 
+	 * @param connections connections the traces for which to retrieve the targets
+	 * @param func        the function to retrieve the EObjects; can
+	 *                    be @code{Connection::getOrigins}
+	 *                    or @code{Connection::getTargets}
+	 * @return a sorted list of {@code EObject} instances
+	 */
+	private Set<EObject> getSortedSetOfArtifacts(List<Connection> connections,
+			Function<? super Connection, ? extends List<EObject>> func) {
+		List<EObject> inserted = connections.stream().map(func).flatMap(Collection::stream)
+				.collect(Collectors.toList());
+		Collections.sort(inserted, new ArtifactHandlerClassNameComparator());
+		return new LinkedHashSet<>(inserted);
+	}
+
+	/**
+	 * Returns all unique origin artifacts of the connections, sorted by the name of
+	 * the {@link IArtifactHandler} that deals with them.
+	 * 
+	 * @param connections the traces for which to retrieve the targets
+	 * @return a sorted list of {@code EObject} instances
+	 */
+	private Set<EObject> getTraceOrigins(List<Connection> connections) {
+		return getSortedSetOfArtifacts(connections, Connection::getOrigins);
+	}
+
+	/**
+	 * Returns all unique target artifacts of the connections, sorted by the name of
+	 * the {@link IArtifactHandler} that deals with them.
+	 * 
+	 * @param connections the traces for which to retrieve the targets
+	 * @return a sorted list of {@code EObject} instances
+	 */
+	private Set<EObject> getTraceTargets(List<Connection> connections) {
+		return getSortedSetOfArtifacts(connections, Connection::getTargets);
+	}
 
 	/**
 	 * Creates a new data provider for the traceability matrix.
@@ -58,12 +145,20 @@ public class TraceabilityMatrixDataProvider implements IDataProvider {
 	 */
 	public TraceabilityMatrixDataProvider(List<Connection> connections, EObject traceModel,
 			ITraceabilityInformationModelAdapter traceAdapter) {
-		for (EObject element : TraceHelper.getTracedElements(connections)) {
-			EntryData colEntry = new EntryData(element);
-			colEntry.connections = traceAdapter.getConnectedElements(element, traceModel);
-			this.columns.add(colEntry);
-			EntryData rowEntry = new EntryData(element);
-			this.rows.add(rowEntry);
+		// Rows contain the origins
+		for (EObject element : this.getTraceOrigins(connections)) {
+			if (element != null) {
+				EntryData rowEntry = new EntryData(element);
+				rowEntry.connections = traceAdapter.getConnectedElements(element, traceModel);
+				this.rows.add(rowEntry);
+			}
+		}
+		// Columns contain the targets
+		for (EObject element : this.getTraceTargets(connections)) {
+			if (element != null) {
+				EntryData colEntry = new EntryData(element);
+				this.columns.add(colEntry);
+			}
 		}
 	}
 
@@ -142,10 +237,10 @@ public class TraceabilityMatrixDataProvider implements IDataProvider {
 	public Connection getCellConnection(int column, int row) {
 		EntryData colEntry = columns.get(column);
 		EntryData rowEntry = rows.get(row);
-		for (Connection connection : colEntry.connections) {
+		for (Connection connection : rowEntry.connections) {
 			for (EObject target : connection.getTargets()) {
-				if (!EMFHelper.hasSameIdentifier(colEntry.artifact, target)
-						&& EMFHelper.hasSameIdentifier(rowEntry.artifact, target)) {
+				if (!EMFHelper.hasSameIdentifier(rowEntry.artifact, target)
+						&& EMFHelper.hasSameIdentifier(colEntry.artifact, target)) {
 					return connection;
 				}
 			}
